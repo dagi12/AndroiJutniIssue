3d2ed2e096dd1df12182d259e88585feb589a46e
Eryk Mariankowski <dagi12@o2.pl>
2018-05-20 13:33:47 +0200

    initial commit

---
+++ app/src/test/java/com/github/androidjunitkotlinissue/ExampleUnitTestJava.java
@@ -0,0 +1,17 @@
+package com.github.androidjunitkotlinissue;
+
+import org.junit.Test;
+
+import static junit.framework.Assert.assertEquals;
+
+/**
+ * @author Eryk Mariankowski <eryk.mariankowski@247.codes> on 20.05.18.
+ */
+public class ExampleUnitTestJava {
+
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
+
+}
